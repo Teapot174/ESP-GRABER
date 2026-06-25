852a3918d54151ac9dfc58a83b4e412c1295a8c7
cheester28 <ztkac42@gmail.com>
2026-06-25 22:25:23 +0300

    switched to Platformio

---
+++ include/config.h
@@ -0,0 +1,19 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// Пины
+#define OLED_SCL 22
+#define OLED_SDA 21
+
+#define BTN_UP 27   // K1 (Up)
+#define BTN_DOWN 26 // K2 (Down)
+#define BTN_OK 33   // K3 (OK)
+#define BTN_BACK 32 // K4 (Back)
+
+#define CC1101_GDO0 2
+#define CC1101_CS 5
+#define CC1101_SCK 18
+#define CC1101_MOSI 23
+#define CC1101_MISO 19
+
+#endif
